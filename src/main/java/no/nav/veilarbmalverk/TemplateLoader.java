f0c2c9350b522cda4d7dba1b39d8b48822b8177f
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-06-09 14:57:43 +0200

    Manuelt les filer på template folderen

--- src/main/java/no/nav/veilarbmalverk/TemplateLoader.java
+++ src/main/java/no/nav/veilarbmalverk/TemplateLoader.java
@@ -1,13 +1,15 @@
 package no.nav.veilarbmalverk;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.*;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -38,44 +40,19 @@ public class TemplateLoader {
     }
 
     private static Map<String, String> readAllFiles() {
-        return Optional.ofNullable(VeilarbmalverkApp.class.getClassLoader().getResource("templates"))
-                .map(URL::getFile)
-                .map(File::new)
-                .map(TemplateLoader::listFiles)
-                .orElseGet(Collections::emptyList)
-                .stream()
-                .collect(Collectors.toMap(TemplateLoader::findName, TemplateLoader::readFile));
+       return Map.of(
+               "cv_jobbprofil_aktivitet", getResourceFileAsString("templates/cv_jobbprofil_aktivitet.json"),
+               "jobbsokerkompetanse_aktivitet", getResourceFileAsString("templates/jobbsokerkompetanse_aktivitet.json"),
+               "soke_jobber_aktivitet", getResourceFileAsString("templates/soke_jobber_aktivitet.json")
+       );
     }
 
-    private static String findName(File file) {
-        String filenameWithExtension = file.getName();
-        int extensionIndex = filenameWithExtension.lastIndexOf(".");
-
-        return filenameWithExtension.substring(0, extensionIndex);
-    }
-
-    private static String readFile(File file) {
-        try {
-            return new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
-        } catch (Exception e) {
-            log.error("Reading file", e);
-            return "";
+    @SneakyThrows
+    private static String getResourceFileAsString(String fileName) {
+        ClassLoader classLoader = VeilarbmalverkApp.class.getClassLoader();
+        try (InputStream resourceStream = classLoader.getResourceAsStream(fileName)) {
+            return new String(resourceStream.readAllBytes(), StandardCharsets.UTF_8);
         }
-    }
-
-    private static List<File> listFiles(File directory) {
-        return Optional.ofNullable(directory.listFiles())
-                .map(List::of)
-                .orElse(Collections.emptyList())
-                .stream()
-                .flatMap((file) -> {
-                    if (file.isFile()) {
-                        return List.of(file).stream();
-                    } else {
-                        return listFiles(directory).stream();
-                    }
-                })
-                .collect(Collectors.toList());
     }
 
     private static void ensureLoaded() {
