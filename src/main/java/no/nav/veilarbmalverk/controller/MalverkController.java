617421eb6f8ab5b8a249fcc5e709627d5201a809
Giil <mads.lee.giil@nav.no>
2025-10-03 11:15:27 +0200

    Legg til logging av bruk av endepunkter

--- src/main/java/no/nav/veilarbmalverk/controller/MalverkController.java
+++ src/main/java/no/nav/veilarbmalverk/controller/MalverkController.java
@@ -3,6 +3,8 @@ package no.nav.veilarbmalverk.controller;
 import no.nav.veilarbmalverk.Extrapolator;
 import no.nav.veilarbmalverk.FilterUtils;
 import no.nav.veilarbmalverk.TemplateLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
 public class MalverkController {
 
     private Extrapolator extrapolator = new Extrapolator();
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping
     public List<String> getTemplateList() {
@@ -27,6 +30,7 @@ public class MalverkController {
 
     @GetMapping("/{name}")
     public Map<String, Object> getTemplateExtrapolated(@PathVariable("name") String name) {
+        logger.info("Kall mot getTemplateExtrapolated");
         return TemplateLoader.get(name)
                 .map(this::extrapolate)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -34,12 +38,14 @@ public class MalverkController {
 
     @GetMapping("/{name}/raw")
     public Map<String, Object> getTemplate(@PathVariable("name") String name) {
+        logger.info("Kall mot getTemplate");
         return TemplateLoader.get(name)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
     public List<Map<String, Object>> filter(@RequestBody Map<String, Object> filter) {
+        logger.info("Kall mot filter");
         Predicate<Map<String, Object>> predicate = FilterUtils.createFilter(filter);
         return TemplateLoader.list()
                 .stream()
